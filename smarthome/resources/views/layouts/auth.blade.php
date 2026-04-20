fbafdffb2c182dd66711922672fca286350261ef
growthman7 <marcaurelbrouzro@gmail.com>
2026-04-20 00:59:38 +0000

    views modifications

--- smarthome/resources/views/layouts/auth.blade.php
+++ smarthome/resources/views/layouts/auth.blade.php
@@ -7,13 +7,11 @@
     {{-- tailwindcss cdn --}}
     <script src="https://cdn.tailwindcss.com"></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @push('styles')
-    @endpush
+    @stack('styles')
 </head>
 <body class="bg-gray-900 text-white">
     <div class="min-h-screen">
         @yield('content')
     </div>
-    @push('scripts')
-    @endpush
+    @stack('scripts')
 </body>
